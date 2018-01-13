dd5c6d7dcf0764bc69d6c65abdade491b30d06e9
thohoh <itrefak@gmail.com>
2018-01-13 20:10:48 +0200

    Users mapping contract

---
+++ contracts/users-mapping.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.18;
+
+contract Users {
+    struct User {
+        string fName;
+        string lName;
+        uint age;
+    }
+
+    mapping(address => User) users;
+    address[] userAddresses;
+
+    function registerUser(string _fName, string _lName, uint _age) public {
+        var newUser = users[msg.sender];
+
+        newUser.fName = _fName;
+        newUser.lName = _lName;
+        newUser.age = _age;
+
+        userAddresses.push(msg.sender);
+    }
+
+    function getUsers() public constant returns (address[]) {
+        return userAddresses;
+    }
+
+    function getUser() public constant returns (string, uint) {
+        return (users[msg.sender].fName, users[msg.sender].age);
+    }
+
+}
